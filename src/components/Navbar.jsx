5f9d46bde88926e418ceaebecbccc88a89c1d4ac
metatony <ibegbulam.ua@gmail.com>
2025-08-18 00:34:34 +0100

    Refactor Navbar and SectionOne components to use semantic HTML elements; replace <nav> with <header> and <header> with <section>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,67 +4,69 @@ import { searchIcon } from "../constants/icons";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg sticky-top ">
-      <div className="container-fluid my-1">
-        <a className="navbar-brand" href="#">
-          Metatony Blog
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div
-          className="collapse navbar-collapse justify-content-between"
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto d-md-flex gap-md-4 fw-light">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/about">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact
-              </a>
-            </li>
-          </ul>
+    <header>
+      <nav className="navbar navbar-expand-lg sticky-top ">
+        <div className="container-fluid my-1">
+          <a className="navbar-brand" href="#">
+            Metatony Blog
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
           <div
-            className="d-none d-md-flex align-items-center justify-content-center gap-3 fw-light"
-            role="search"
+            className="collapse navbar-collapse justify-content-between"
+            id="navbarSupportedContent"
           >
-            <span>Search</span>
-            <img width={17} height={17} src={searchIcon} alt="search icon" />
-          </div>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto d-md-flex gap-md-4 fw-light">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
 
-          <form className="d-flex d-md-none" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+            <div
+              className="d-none d-md-flex align-items-center justify-content-center gap-3 fw-light"
+              role="search"
+            >
+              <span>Search</span>
+              <img width={17} height={17} src={searchIcon} alt="search icon" />
+            </div>
+
+            <form className="d-flex d-md-none" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
--- src/pages/Home/sections/SectionOne.jsx
+++ src/pages/Home/sections/SectionOne.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const SectionOne = () => {
   return (
-    <header className="header d-flex flex-column align-items-center justify-content-center text-center">
+    <section className="header d-flex flex-column align-items-center justify-content-center text-center">
       <p className="blog-name fw-normal">Metatony Blog</p>
       <p className="header-title fw-semibold">Stories & Ideas</p>
       <p className="header-description fw-light">
         Explore breathtaking landscapes, iconic landmarks, and hidden gems
         around the globe
       </p>
-    </header>
+    </section>
   );
 };
 
